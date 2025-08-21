fa64566efaac156e68893b28d5601a96d6a4870b
Dmitriy Sinelnikov <dmitriy.sinelnikoff@gmail.com>
2025-08-21 22:04:02 +0500

    sliders

--- src/main.js
+++ src/main.js
@@ -114,10 +114,19 @@ loop.updatables.push(bottleFinder)
 
 
 
+// SWIPER BITCH SOSI KIRPI4
+import Swiper from 'swiper';
+import { EffectCards } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/effect-cards';
 
-
-
-
-
-
-
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll(".swiper").forEach((el) => {
+    new Swiper(el, {
+      modules: [EffectCards],
+      effect: "cards",
+      grabCursor: true,
+      loop: true
+    });
+  });
+});
